70a01d73b82c997cd91a909469472e4c8c1644d0
yuriy.orishchuk <yuriy.oryshchuk@gmail.com>
2025-05-30 21:13:19 +0300

    add femap userData workflow

--- modelDescr.py
+++ modelDescr.py
@@ -12,41 +12,40 @@ import comtypes.client
 tlb_path = r"C:\Program Files\Siemens\Femap 2406\femap.tlb"
 comtypes.client.GetModule(tlb_path)
 
-# existObj = pythoncom.connect(Pyfemap.model.CLSID)
-# femap = Pyfemap.model(existObj)
+existObj = pythoncom.connect(Pyfemap.model.CLSID)
+femap = Pyfemap.model(existObj)
+userData = femap.feUserData
+rc = femap.feAppMessage(feConstants.FCM_NORMAL, "Connected!")
+USER_DATA_TITLE = 'user_messages'
 
-# rc = femap.feAppMessage(feConstants.FCM_NORMAL, "Status")
-# userData = femap.feUserData
-# dataTitle = "myData"
-
-def setUserData():
-    userText = "fdfdf \n fdffdf \n dsfdfdf df df dfsfsd11111111111111 ff2234 32 sdf sfd fsd fsd f"
+def setUserData(userText, title):
+    rc = userData.DeleteTitle(title)
     rc = userData.WriteString(userText)
-    rc = userData.PutTitle(dataTitle)
+    rc = userData.PutTitle(title)
     print({rc})
 
 
-def getUserData(title):
+def getFemModelUserMsg(title):
     rc = userData.GetTitle(title)
     print(rc)
     [rc, uData] = userData.ReadString()    
-    print(uData)
-    femap.feAppMessage(feConstants.FCM_HIGHLIGHT, uData)
+    return uData
 
-def createInputTextBox():
-    print("Creating input text box")
-    canvas = tk.Canvas(root, height=800, width = 600, bg = "white")
-    canvas.pack()
-    string_box = tk.Entry(root)
-    canvas.create_window(250, 125, window = string_box)
-    input_string = string_box.get()
-    print(f"Input string: {input_string}")
+# def createInputTextBox():
+#     print("Creating input text box")
+#     canvas = tk.Canvas(root, height=800, width = 600, bg = "white")
+#     canvas.pack()
+#     string_box = tk.Entry(root)
+#     canvas.create_window(250, 125, window = string_box)
+#     input_string = string_box.get()
+#     print(f"Input string: {input_string}")
 
 def addText(event=None, entry=None, textArea=None):
     text = entry.get()
     if text:
+        dateTimeStr = datetime.datetime.now().strftime("%Y-%m-%d %H-%M")
         textArea.config(state=tk.NORMAL)
-        textArea.insert(tk.END, '\n' + text)
+        textArea.insert(tk.END, '\n'  + dateTimeStr + ': '+ text)
         entry.delete(0, tk.END)
         print("Adding text")
     else: 
@@ -65,21 +64,20 @@ def saveTextArea(textArea=None):
     userText = textArea.get("1.0", tk.END).strip()
     if userText:
         print(userText)
-        # rc = userData.WriteString(userText)
-        # rc = userData.PutTitle(dataTitle)
-        # print(f"User data saved with title '{dataTitle}': {userText}")
+        setUserData(userText, USER_DATA_TITLE)
     else:
         print("No text to save")
 
-
-def uiPanel():
+def uiPanel(userMsgs):
     root = tk.Tk()
-    root.title("User Description For Femap Model")
+    root.title("User messages for current Femap model")
     #root.geometry("400x400")
     root.columnconfigure(0, weight=1)
     #root.columnconfigure(1, weight=1)
-    #root.rowconfigure(0, weight=1)
-    # root.rowconfigure(1, weight=1)
+    root.rowconfigure(0, weight=1)
+    root.rowconfigure(1, weight=1)
+    #root.rowconfigure(2, weight=1)
+    #root.grid_rowconfigure(1, weight=1)
 
     frame = tk.Frame(root)
     frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
@@ -89,9 +87,9 @@ def uiPanel():
     frame.rowconfigure(0, weight=1)
         
     textArea = scrolledtext.ScrolledText(frame)
-    textArea.insert(tk.END, "Enter your description here...\n sdsdsdsd аываыа ыаыв ааыва ")
+    textArea.insert(tk.END, userMsgs)
     textArea.config(state = "disabled")
-    textArea.grid(row=1, column=0, columnspan=2, sticky="new", padx=5, pady=5)
+    textArea.grid(row=1, column=0, columnspan=2, sticky="snew", padx=5, pady=5)
 
     editBtn = tk.Button(frame, text = "Edit", command = lambda: editTextArea(textArea))
     editBtn.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
@@ -100,28 +98,16 @@ def uiPanel():
     saveBtn.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
     
     entry = tk.Entry(frame)
-    entry.grid(row=2, column=0, sticky='sew', padx=5, pady=5)
+    entry.grid(row=2, column=0, columnspan=2, sticky='sew', padx=5, pady=5)
+    entry.focus()
     entry.bind(("<Return>"), lambda event: addText(event, entry, textArea))
     
     addBtn = tk.Button(frame, text = "Add", command = lambda: addText(None, entry, textArea))
-    addBtn.grid(row=2, column=1, padx=5, pady=5, sticky='w')
-    
-
-    # label = tk.Label(root, text="Enter your description:")
-
-    # label.configure(font=("Arial", 14), text="mymymyModel User Description")
-    # label.pack()
-
-    # label1 = tk.Entry(root)
-    # label1.insert(0, float(x1)**0.5)
-    # label1["state"] = "readonly
-
-    # InputBox = tk.Button(root, text = "Input Box", padx=10,pady=10,
-    #                 fg = "white", bg ="blue", command = createInputTextBox)
-    # InputBox.pack()
-
+    addBtn.grid(row=2, column=2, padx=5, pady=5, sticky='we')
+   
     root.mainloop()
 
-uiPanel()
+userMsgs = getFemModelUserMsg(USER_DATA_TITLE)
+uiPanel(userMsgs)
 #setUserData()
 #getUserData(dataTitle)
